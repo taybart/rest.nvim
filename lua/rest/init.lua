afd471785045bc42705118c3b3f77897d12fa38e
Taylor Bartlett <taybart@gmail.com>
2023-02-12 12:40:26 -0700

    fix: add filetype detection

--- lua/rest/init.lua
+++ lua/rest/init.lua
@@ -2,15 +2,22 @@ local M = {}
 
 local execute = require('rest.execute')
 
+local function has_plenary()
+  local exists, _ = pcall(require, 'plenary')
+  return exists
+end
+
 function M.setup()
   local group = 'taybart.rest'
+
+  vim.filetype.add({ extension = { rest = 'hcl' } })
 
   vim.api.nvim_create_augroup(group, {})
   vim.api.nvim_create_autocmd('BufRead,BufNewFile', {
     group = group,
     pattern = '*.rest',
     callback = function()
-      vim.opt.filetype = 'hcl'
+      -- vim.opt.filetype = 'hcl'
 
       vim.api.nvim_create_user_command('ExecuteBlock', execute.block, { nargs = '?' })
       vim.api.nvim_create_user_command(
@@ -24,6 +31,10 @@ function M.setup()
       vim.keymap.set('n', '<c-t>', execute.file, {})
     end,
   })
+
+  if has_plenary() then
+    require('plenary.filetype').add_file('rest')
+  end
 end
 
 return M
