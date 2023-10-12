6d2d5b535aac338949c72b9a6baaf1b7b9da6821
Taylor Bartlett <taybart@gmail.com>
2023-10-12 15:52:16 -0600

    fix: breaking change

--- lua/rest/init.lua
+++ lua/rest/init.lua
@@ -13,11 +13,12 @@ function M.setup()
   vim.filetype.add({ extension = { rest = 'hcl' } })
 
   vim.api.nvim_create_augroup(group, {})
-  vim.api.nvim_create_autocmd('BufRead,BufNewFile', {
+  vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
     group = group,
     pattern = '*.rest',
     callback = function()
       -- vim.opt.filetype = 'hcl'
+      vim.bo.commentstring = '# %s'
 
       vim.api.nvim_create_user_command('ExecuteBlock', execute.block, { nargs = '?' })
       vim.api.nvim_create_user_command(
