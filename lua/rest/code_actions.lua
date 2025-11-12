d2d99e45accce5eeb51efd91c9a9a5d46c726e5f
Taylor Bartlett <taybart@gmail.com>
2025-11-12 10:54:02 -0700

    fix: don't clobber default code-actions

--- lua/rest/code_actions.lua
+++ lua/rest/code_actions.lua
@@ -7,18 +7,21 @@ function M.register_code_actions()
   end
   require("code-actions").setup({
     register_keymap = false,
-    name = "rest.nvim",
-    filetypes = { include = { "rest" } },
-    -- stylua: ignore
-    actions = {
-      {
-        command = 'Execute block under cursor',
-        show = function() return require("rest/execute").block_label_under_cursor() ~= nil end,
-        fn = function() require("rest/execute").block_under_cursor() end,
-      },
-      {
-        command = 'Pick label to run',
-        fn = function() require('rest/execute').do_labels() end,
+    servers = {
+      ["rest.nvim"] = {
+        filetypes = { include = { "rest" } },
+        -- stylua: ignore
+        actions = {
+          {
+            command = 'Execute block under cursor',
+            show = function() return require("rest/execute").block_label_under_cursor() ~= nil end,
+            fn = function() require("rest/execute").block_under_cursor() end,
+          },
+          {
+            command = 'Pick label to run',
+            fn = function() require('rest/execute').do_labels() end,
+          },
+        },
       },
     },
   })
