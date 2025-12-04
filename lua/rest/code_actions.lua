df5705221b5d67d07c131214e5e2a8c6b8e9e769
Taylor Bartlett <taybart@gmail.com>
2025-12-04 06:58:30 -0700

    fix: use add_server

--- lua/rest/code_actions.lua
+++ lua/rest/code_actions.lua
@@ -5,23 +5,19 @@ function M.register_code_actions()
   if not has_code_actions then
     return
   end
-  require("code-actions").setup({
-    register_keymap = false,
-    servers = {
-      ["rest.nvim"] = {
-        filetypes = { include = { "rest" } },
-        -- stylua: ignore
-        actions = {
-          {
-            command = 'Execute block under cursor',
-            show = function() return require("rest/execute").block_label_under_cursor() ~= nil end,
-            fn = function() require("rest/execute").block_under_cursor() end,
-          },
-          {
-            command = 'Pick label to run',
-            fn = function() require('rest/execute').do_labels() end,
-          },
-        },
+  require("code-actions").add_server({
+    name = "rest.nvim",
+    filetypes = { include = { "rest" } },
+    -- stylua: ignore
+    actions = {
+      {
+        command = 'Execute block under cursor',
+        show = function() return require("rest/execute").block_label_under_cursor() ~= nil end,
+        fn = function() require("rest/execute").block_under_cursor() end,
+      },
+      {
+        command = 'Pick label to run',
+        fn = function() require('rest/execute').do_labels() end,
       },
     },
   })
